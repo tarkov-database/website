820d45cea84ea32c5d2f8f3f8ba467a012b15189
Markus Wiegand <mail@morphy2k.dev>
2019-10-31 02:14:10 +0100

    refactor template parsing

--- view/view.go
+++ view/view.go
@@ -1,27 +1,27 @@
 package view
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"net/http"
 	"strings"
+	"sync"
 
 	"github.com/tarkov-database/website/model/item"
 
 	"github.com/google/logger"
 )
 
+var errTemplateNotExist = errors.New("template doesn't exist")
+
+const templateDir = "view/templates"
+
 var (
 	templates map[string]*template.Template
-
-	errTemplateNotExist = errors.New("template doesn't exist")
-)
-
-func init() {
-	templates = make(map[string]*template.Template)
-
-	funcMap := template.FuncMap{
+	funcMap   = template.FuncMap{
 		// Helpers
 		"addFloat":         addFloat,
 		"subtractFloat":    subtractFloat,
@@ -42,93 +42,46 @@ func init() {
 		"resolveItemList":        resolveItemList,
 		"resolveSlots":           resolveSlots,
 	}
+)
 
-	// General
-	templates["index"] = template.Must(template.New("index").Funcs(funcMap).ParseFiles(
-		"view/templates/index/index.gohtml", "view/templates/index/base.gohtml",
-		"view/templates/index/head.gohtml", "view/templates/header.gohtml", "view/templates/footer.gohtml"))
-	templates["list"] = template.Must(template.New("list").Funcs(funcMap).ParseFiles(
-		"view/templates/list/list.gohtml", "view/templates/list/base.gohtml",
-		"view/templates/list/head.gohtml", "view/templates/header.gohtml", "view/templates/footer.gohtml"))
+func init() {
+	f, err := ioutil.ReadFile(templateDir + "/index.json")
+	if err != nil {
+		logger.Fatal(err)
+	}
 
-	// Item
-	templates["item_common"] = template.Must(template.New("item_common").Funcs(funcMap).ParseFiles(
-		"view/templates/item/item.gohtml", "view/templates/item/item_common.gohtml",
-		"view/templates/item/base.gohtml", "view/templates/item/head.gohtml", "view/templates/header.gohtml", "view/templates/footer.gohtml"))
-	templates["item_ammunition"] = template.Must(template.New("item_ammunition").Funcs(funcMap).ParseFiles(
-		"view/templates/item/item.gohtml", "view/templates/item/item_ammunition.gohtml",
-		"view/templates/item/base.gohtml", "view/templates/item/head.gohtml", "view/templates/header.gohtml", "view/templates/footer.gohtml"))
-	templates["item_firearm"] = template.Must(template.New("item_firearm").Funcs(funcMap).ParseFiles(
-		"view/templates/item/item.gohtml", "view/templates/item/item_firearm.gohtml",
-		"view/templates/item/base.gohtml", "view/templates/item/head.gohtml", "view/templates/header.gohtml", "view/templates/footer.gohtml"))
-	templates["item_melee"] = template.Must(template.New("item_melee").Funcs(funcMap).ParseFiles(
-		"view/templates/item/item.gohtml", "view/templates/item/item_melee.gohtml",
-		"view/templates/item/base.gohtml", "view/templates/item/head.gohtml", "view/templates/header.gohtml", "view/templates/footer.gohtml"))
-	templates["item_magazine"] = template.Must(template.New("item_magazine").Funcs(funcMap).ParseFiles(
-		"view/templates/item/item.gohtml", "view/templates/item/item_magazine.gohtml",
-		"view/templates/item/base.gohtml", "view/templates/item/head.gohtml", "view/templates/header.gohtml", "view/templates/footer.gohtml"))
-	templates["item_modification"] = template.Must(template.New("item_modification").Funcs(funcMap).ParseFiles(
-		"view/templates/item/item.gohtml", "view/templates/item/item_modification.gohtml",
-		"view/templates/item/base.gohtml", "view/templates/item/head.gohtml", "view/templates/header.gohtml", "view/templates/footer.gohtml"))
-	templates["item_grenade"] = template.Must(template.New("item_grenade").Funcs(funcMap).ParseFiles(
-		"view/templates/item/item.gohtml", "view/templates/item/item_grenade.gohtml",
-		"view/templates/item/base.gohtml", "view/templates/item/head.gohtml", "view/templates/header.gohtml", "view/templates/footer.gohtml"))
-	templates["item_armor"] = template.Must(template.New("item_armor").Funcs(funcMap).ParseFiles(
-		"view/templates/item/item.gohtml", "view/templates/item/item_armor.gohtml",
-		"view/templates/item/base.gohtml", "view/templates/item/head.gohtml", "view/templates/header.gohtml", "view/templates/footer.gohtml"))
-	templates["item_backpack"] = template.Must(template.New("item_backpack").Funcs(funcMap).ParseFiles(
-		"view/templates/item/item.gohtml", "view/templates/item/item_backpack.gohtml",
-		"view/templates/item/base.gohtml", "view/templates/item/head.gohtml", "view/templates/header.gohtml", "view/templates/footer.gohtml"))
-	templates["item_clothing"] = template.Must(template.New("item_clothing").Funcs(funcMap).ParseFiles(
-		"view/templates/item/item.gohtml", "view/templates/item/item_clothing.gohtml",
-		"view/templates/item/base.gohtml", "view/templates/item/head.gohtml", "view/templates/header.gohtml", "view/templates/footer.gohtml"))
-	templates["item_tacticalrig"] = template.Must(template.New("item_tacticalrig").Funcs(funcMap).ParseFiles(
-		"view/templates/item/item.gohtml", "view/templates/item/item_tacticalrig.gohtml",
-		"view/templates/item/base.gohtml", "view/templates/item/head.gohtml", "view/templates/header.gohtml", "view/templates/footer.gohtml"))
-	templates["item_medical"] = template.Must(template.New("item_medical").Funcs(funcMap).ParseFiles(
-		"view/templates/item/item.gohtml", "view/templates/item/item_medical.gohtml",
-		"view/templates/item/base.gohtml", "view/templates/item/head.gohtml", "view/templates/header.gohtml", "view/templates/footer.gohtml"))
-	templates["item_food"] = template.Must(template.New("item_food").Funcs(funcMap).ParseFiles(
-		"view/templates/item/item.gohtml", "view/templates/item/item_food.gohtml",
-		"view/templates/item/base.gohtml", "view/templates/item/head.gohtml", "view/templates/header.gohtml", "view/templates/footer.gohtml"))
-	templates["item_container"] = template.Must(template.New("item_container").Funcs(funcMap).ParseFiles(
-		"view/templates/item/item.gohtml", "view/templates/item/item_container.gohtml",
-		"view/templates/item/base.gohtml", "view/templates/item/head.gohtml", "view/templates/header.gohtml", "view/templates/footer.gohtml"))
-	templates["item_headphone"] = template.Must(template.New("item_headphone").Funcs(funcMap).ParseFiles(
-		"view/templates/item/item.gohtml", "view/templates/item/item_headphone.gohtml",
-		"view/templates/item/base.gohtml", "view/templates/item/head.gohtml", "view/templates/header.gohtml", "view/templates/footer.gohtml"))
+	index := make(map[string][]string)
+	if err = json.Unmarshal(f, &index); err != nil {
+		logger.Fatal(err)
+	}
 
-	// Table
-	templates["table_generic"] = template.Must(template.New("table_generic").Funcs(funcMap).ParseFiles(
-		"view/templates/table/table_generic.gohtml", "view/templates/table/base.gohtml",
-		"view/templates/table/head.gohtml", "view/templates/header.gohtml", "view/templates/footer.gohtml"))
-	templates["table_ammunition"] = template.Must(template.New("table_ammunition").Funcs(funcMap).ParseFiles(
-		"view/templates/table/table_ammunition.gohtml", "view/templates/table/base.gohtml",
-		"view/templates/table/head.gohtml", "view/templates/header.gohtml", "view/templates/footer.gohtml"))
-	templates["table_armor"] = template.Must(template.New("table_armor").Funcs(funcMap).ParseFiles(
-		"view/templates/table/table_armor.gohtml", "view/templates/table/base.gohtml",
-		"view/templates/table/head.gohtml", "view/templates/header.gohtml", "view/templates/footer.gohtml"))
-	templates["table_magazine"] = template.Must(template.New("table_magazine").Funcs(funcMap).ParseFiles(
-		"view/templates/table/table_magazine.gohtml", "view/templates/table/base.gohtml",
-		"view/templates/table/head.gohtml", "view/templates/header.gohtml", "view/templates/footer.gohtml"))
+	templates = make(map[string]*template.Template)
 
-	// Status
-	templates["status_404"] = template.Must(template.New("status_404").Funcs(funcMap).ParseFiles(
-		"view/templates/status/404.gohtml", "view/templates/status/base.gohtml",
-		"view/templates/status/head.gohtml", "view/templates/header.gohtml", "view/templates/footer.gohtml"))
-	templates["status_500"] = template.Must(template.New("status_500").Funcs(funcMap).ParseFiles(
-		"view/templates/status/500.gohtml", "view/templates/status/base.gohtml",
-		"view/templates/status/head.gohtml", "view/templates/header.gohtml", "view/templates/footer.gohtml"))
-	templates["status_503"] = template.Must(template.New("status_503").Funcs(funcMap).ParseFiles(
-		"view/templates/status/503.gohtml", "view/templates/status/base.gohtml",
-		"view/templates/status/head.gohtml", "view/templates/header.gohtml", "view/templates/footer.gohtml"))
+	ch := make(chan *template.Template, 1)
+	wg := &sync.WaitGroup{}
+	wg.Add(len(index))
 
-	templates["about"] = template.Must(template.New("about").Funcs(funcMap).ParseFiles(
-		"view/templates/about/about.gohtml", "view/templates/about/base.gohtml",
-		"view/templates/about/head.gohtml", "view/templates/header.gohtml", "view/templates/footer.gohtml"))
-	templates["projects"] = template.Must(template.New("projects").Funcs(funcMap).ParseFiles(
-		"view/templates/projects/projects.gohtml", "view/templates/projects/base.gohtml",
-		"view/templates/projects/head.gohtml", "view/templates/header.gohtml", "view/templates/footer.gohtml"))
+	for k, v := range index {
+		go loadTemplate(k, v, ch, wg)
+	}
+
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	for t := range ch {
+		templates[t.Name()] = t
+	}
+}
+
+func loadTemplate(name string, files []string, ch chan *template.Template, wg *sync.WaitGroup) {
+	for i, k := range files {
+		files[i] = templateDir + k
+	}
+
+	ch <- template.Must(template.New(name).Funcs(funcMap).ParseFiles(files...))
+	wg.Done()
 }
 
 func Render(t string, d interface{}, w http.ResponseWriter) {
